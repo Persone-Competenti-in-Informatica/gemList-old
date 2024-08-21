8e0343d00393695878adbf83dfbe264b7ee8774e
ReLoia <zrennyz@gmail.com>
2024-08-21 22:59:28 +0200

    Recreating the whole app to work with Pinia

---
+++ frontend/backup/src/main.js
@@ -0,0 +1,60 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+// import { createStore } from 'vuex'
+
+import './style.css'
+import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+    { path: '/', component: () => import('./components/Home.vue') },
+    { path: '/game/:id', component: () => import('./components/Game.vue') },
+
+//     TODO: change the path to the correct components
+    { path: '/explore', component: () => import('./components/PageNotFound.vue') },
+    { path: '/calendar', component: () => import('./components/PageNotFound.vue') },
+    { path: '/library', component: () => import('./components/PageNotFound.vue') },
+    { path: '/community', component: () => import('./components/PageNotFound.vue') },
+    { path: '/friends', component: () => import('./components/PageNotFound.vue') },
+
+    { path: '/login', component: () => import('./components/PageNotFound.vue') },
+    { path: '/register', component: () => import('./components/PageNotFound.vue') },
+    { path: '/profile', component: () => import('./components/PageNotFound.vue') },
+
+    { path: '/:pathMatch(.*)*', component: () => import('./components/PageNotFound.vue') },
+
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+// const store = createStore({
+//     state() {
+//         return {
+//             // only the username and avatar are stored because all the other stuff page related
+//             // like the games and the user's friends are loaded from the server when the page is loaded
+//             user: {
+//                 username: '',
+//                 avatar: ''
+//             }
+//         }
+//     },
+//     mutations: {
+//         setUser(state, userdata) {
+//             state.user = userdata
+//         },
+//     },
+//     actions: {
+//         setUser(context, userdata) {
+//             context.commit('setUser', userdata)
+//         },
+//     }
+// })
+
+const pinia = createPinia()
+
+createApp(App)
+    .use(router)
+    .use(pinia)
+    .mount('#app');
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,57 +1,36 @@
 import { createApp } from 'vue'
-import { createStore } from 'vuex'
+import { createPinia } from 'pinia'
 
 import './style.css'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-    { path: '/', component: () => import('./components/Home.vue') },
-    { path: '/game/:id', component: () => import('./components/Game.vue') },
+    { path: '/', component: () => import('./components/home/Home.vue') },
+    { path: '/game/:id', component: () => import('./components/game/Game.vue') },
 
-//     TODO: change the path to the correct components
-    { path: '/explore', component: () => import('./components/PageNotFound.vue') },
-    { path: '/calendar', component: () => import('./components/PageNotFound.vue') },
-    { path: '/library', component: () => import('./components/PageNotFound.vue') },
-    { path: '/community', component: () => import('./components/PageNotFound.vue') },
-    { path: '/friends', component: () => import('./components/PageNotFound.vue') },
+    //     TODO: change the path to the correct components
+    { path: '/explore', component: () => import('./components/404/NotFound.vue') },
+    { path: '/calendar', component: () => import('./components/404/NotFound.vue') },
+    { path: '/library', component: () => import('./components/404/NotFound.vue') },
+    { path: '/community', component: () => import('./components/404/NotFound.vue') },
+    { path: '/friends', component: () => import('./components/404/NotFound.vue') },
 
-    { path: '/login', component: () => import('./components/PageNotFound.vue') },
-    { path: '/register', component: () => import('./components/PageNotFound.vue') },
-    { path: '/profile', component: () => import('./components/PageNotFound.vue') },
+    { path: '/login', component: () => import('./components/404/NotFound.vue') },
+    { path: '/register', component: () => import('./components/404/NotFound.vue') },
+    { path: '/profile', component: () => import('./components/404/NotFound.vue') },
 
-    { path: '/:pathMatch(.*)*', component: () => import('./components/PageNotFound.vue') },
-
+    { path: '/:pathMatch(.*)*', component: () => import('./components/404/NotFound.vue') },
 ]
+
 const router = createRouter({
     history: createWebHistory(),
     routes,
 });
 
-const store = createStore({
-    state() {
-        return {
-            // only the username and avatar are stored because all the other stuff page related
-            // like the games and the user's friends are loaded from the server when the page is loaded
-            user: {
-                username: '',
-                avatar: ''
-            }
-        }
-    },
-    mutations: {
-        setUser(state, userdata) {
-            state.user = userdata
-        },
-    },
-    actions: {
-        setUser(context, userdata) {
-            context.commit('setUser', userdata)
-        },
-    }
-})
+const pinia = createPinia()
 
 createApp(App)
     .use(router)
-    .use(store)
-    .mount('#app');
+    .use(pinia)
+    .mount("#app")
